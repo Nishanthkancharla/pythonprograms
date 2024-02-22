5b4a0c64171609ba9246feff7637c84aaa061392
Nishanth Kancharla <126706493+Nishanthkancharla@users.noreply.github.com>
2024-02-22 17:37:46 +0530

    Add files via upload

---
+++ EZTS/DAY-3/prog6.py
@@ -0,0 +1,13 @@
+#program to print an armstrong number using strings and functions
+def armstrong(n,m):
+    
+    
+    for i in range(n,m+1):
+        sum=0
+        s=str(i)
+        for j in s:
+            sum+=int(j)**len(i)
+        if str(sum)==i:
+            print(i)
+n,m=int(input()),int(input())
+armstrong(n,m)           
